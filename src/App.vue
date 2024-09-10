26d58b0d36eec99381f6b27c4a8909facc2423b0
Edilson Junior <etmjr03@gmail.com>
2024-09-09 23:16:08 -0300

    Adicionando menu pc e responsivo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeaderComponent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from './components/HeaderComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComponent
   }
 }
 </script>
 
 <style>
+body{
+  height: 100vh;
+  overflow: hidden;
+  background: #c9a8f5;
+  background: linear-gradient(135deg, #c9a8f5 0%, #0e74f1 88%, #009ddb 100%);
+  background: -moz-linear-gradient(135deg, #c9a8f5 0%, #57a0fa 88%, #009ddb 100%);
+  background: -webkit-linear-gradient(135deg, #c9a8f5 0%, #0e74f1 88%, #009ddb 100%);
+  filter: progid: DXImageTransform.Microsoft.gradient( startColorstr="#CAA8F5", endColorstr="#1075f1", GradientType=1 );
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
+}
+
+@media only screen and (max-width: 800px) {
+  body{
+    background: #FFF;
+  }
 }
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,116 @@
+<template>
+  <div 
+    class="menu-principal" 
+    :class="{'esconder-menu-mobile': exibirMenuMobile}"
+  >
+    <div class="itens-menu">Início</div>
+    <div class="itens-menu">Produtos</div>
+    <div class="itens-menu">Clientes</div>
+    <div class="itens-menu">Pedidos</div>
+  </div>
+  <div @click="exibirMenu" class="item-menu-responsivo">
+    <img src="../assets/images/config.svg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      exibirMenuMobile: true
+    }
+  },
+  methods: {
+    exibirMenu(){
+      this.exibirMenuMobile 
+        ? this.exibirMenuMobile = false 
+        : this.exibirMenuMobile = true 
+    }
+  }
+}
+</script>
+
+<style>
+  .menu-principal{
+    display: flex;
+    width: 750px;
+    height: 50px;
+    margin: 0 auto;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    
+    background: rgba(255, 255, 255, 0.26);
+    border-radius: 16px;
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(6.1px);
+    -webkit-backdrop-filter: blur(6.1px);
+    border: 2px solid rgba(255, 255, 255, 0.3);
+
+    box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+    -webkit-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+    -moz-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+
+    border-radius: 30px;
+  }
+
+  .itens-menu{
+    display: flex;
+    width: 100px;
+    height: 80%;
+    justify-content: center;
+    align-items: center;
+    border-radius: 20px;
+    cursor: pointer;
+
+    color: #FFF;
+    font-weight: 700;
+  }
+
+  .itens-menu:hover{
+    color: #000;
+  }
+
+  .item-menu-responsivo{
+    display: none;
+    width: 50px;
+    height: 50px;
+    padding: 10px;
+    text-align: center;
+
+    background: #0e74f1;
+
+    box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+    -webkit-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+    -moz-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+
+    cursor: pointer;
+    border-radius: 50%;
+  }
+
+  @media only screen and (max-width: 800px) {
+  .menu-principal{
+    width: fit-content;
+    height: fit-content;
+    flex-direction: column;
+    position: absolute;
+    top: 100px;
+    
+    box-shadow: none;
+  }
+
+  .itens-menu{
+    height: 50px;
+    color: #000;
+  }
+
+  .item-menu-responsivo{
+    display: flex;
+    margin-top: 10px;
+  }
+
+  .esconder-menu-mobile{
+    display: none;
+  }
+}
+</style>
